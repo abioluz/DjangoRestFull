1da19492299b7c7a0c4a65e0ca14cf7e6daf0092
Abioluz <abioluz@gmail.com>
2023-08-14 22:25:07 -0300

    Tirando proveito da viewsets

--- cursos/views.py
+++ cursos/views.py
@@ -1,8 +1,17 @@
 from rest_framework import generics
 from rest_framework.generics import get_object_or_404
 
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
+
+'''
+API V1
+'''
+
 
 class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
@@ -20,6 +29,8 @@ class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
+
+
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
@@ -31,3 +42,21 @@ class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
                                      pk=self.kwargs.get('avaliacao_pk'))
         return get_object_or_404(self.get_queryset(),
                                      pk=self.kwargs.get('avaliacao_pk'))
+
+'''
+API V2
+'''
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+    @action(detail=True, methods=['get'])
+    def avaliacoes(self, request, pk=None):
+        curso = self.get_object()
+        serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
+        return Response(serializer.data)
+
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
--- escola/urls.py
+++ escola/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from cursos.urls import router
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
     path('api/v1/', include('cursos.urls')),
+    path('api/v2/', include(router.urls)),
 ]
