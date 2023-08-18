0d2ad22a83887983a67921e71f34819ab1790eac
Abioluz <abioluz@gmail.com>
2023-08-17 21:56:37 -0300

    Resolvendo permissão mais restritiva para que o usuário sem a permição não consiga requerir o GET

--- cursos/permissions.py
+++ cursos/permissions.py
@@ -8,3 +8,24 @@ class EhSuperUser(permissions.BasePermission):
             return False
         return True
 
+class TemPermissao(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        elif request.method == 'DELETE':
+            if 'cursos.delete_curso' in request.user.get_user_permissions():
+                return True
+        elif request.method == 'GET':
+            if 'cursos.view_curso' in request.user.get_user_permissions():
+                return True
+        elif request.method == 'POST':
+            if 'cursos.add_curso' in request.user.get_user_permissions():
+                return True
+        elif request.method == 'PUT':
+            if 'cursos.change_curso' in request.user.get_user_permissions():
+                return True
+        return False
+
+class TemPermissao2(permissions.DjangoObjectPermissions):
+    permissions.DjangoObjectPermissions.perms_map[
+        'GET'] = ['%(app_label)s.view_%(model_name)s']
